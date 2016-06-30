f46856d760711e18a8fab632723bdf3230708cea
Naren Ranjit <narendran.ranjit@gmail.com>
2016-06-29 20:10:45 -0700

    internals -> build

--- server/middlewares/frontendMiddleware.js
+++ server/middlewares/frontendMiddleware.js
@@ -65,7 +65,7 @@ module.exports = (app, options) => {
   if (isProd) {
     addProdMiddlewares(app, options);
   } else {
-    const webpackConfig = require('../../internals/webpack/webpack.dev.babel');
+    const webpackConfig = require('../../build/webpack/webpack.dev.babel');
     addDevMiddlewares(app, webpackConfig);
   }
 
