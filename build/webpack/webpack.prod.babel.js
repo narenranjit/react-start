9bba4ab5f7cb4dfb084bc43cc0f08dbe6ef05f25
Naren Ranjit <narendran.ranjit@gmail.com>
2016-07-14 00:22:20 -0700

    fix sassloader in prod

--- build/webpack/webpack.prod.babel.js
+++ build/webpack/webpack.prod.babel.js
@@ -30,6 +30,7 @@ module.exports = require('./webpack.base.babel')({
 
   // Utilize long-term caching by adding content hashes (not compilation hashes) to compiled assets
   output: {
+    publicPath: './',
     filename: '[name].[chunkhash].js',
     chunkFilename: '[name].[chunkhash].chunk.js',
   },
